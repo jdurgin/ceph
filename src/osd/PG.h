9b80a1e796fcd4d1b4a8463e50d332226408104f
Josh Durgin <jdurgin@redhat.com>
2018-03-07 22:09:19 -0500

    PG, PrimaryLogPG: trim log and rollback info for error log entries
    Regular updates piggyback some osd state for this purpose with
    MOSDRepOp[Reply]. Do the same thing for pure log entry updates (write
    errors and lost/revert additions) via MOSDPGUpdateLogMissing[Reply].
    
    Fixes: http://tracker.ceph.com/issues/22050
    Signed-off-by: Josh Durgin <jdurgin@redhat.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2413,7 +2413,9 @@ public:
 
   bool append_log_entries_update_missing(
     const mempool::osd_pglog::list<pg_log_entry_t> &entries,
-    ObjectStore::Transaction &t);
+    ObjectStore::Transaction &t,
+    boost::optional<eversion_t> trim_to,
+    boost::optional<eversion_t> roll_forward_to);
 
   /**
    * Merge entries updating missing as necessary on all
@@ -2421,7 +2423,9 @@ public:
    */
   void merge_new_log_entries(
     const mempool::osd_pglog::list<pg_log_entry_t> &entries,
-    ObjectStore::Transaction &t);
+    ObjectStore::Transaction &t,
+    boost::optional<eversion_t> trim_to,
+    boost::optional<eversion_t> roll_forward_to);
 
   void reset_interval_flush();
   void start_peering_interval(
